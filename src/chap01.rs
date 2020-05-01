bdf960313cec096ead0d2040674186c9dd921562
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-01 17:02:40 +0900

    finish to question 3

--- src/chap01.rs
+++ src/chap01.rs
@@ -5,3 +5,34 @@ pub fn q1() {
     println!("{}", t);
 
 }
+
+pub fn q2() {
+
+    let s = String::from("パタトクカシーー");
+    let t = s.chars();
+    let mut cnt =0;
+
+    for i in t {
+        if cnt % 2 != 0 {
+            print!("{}", i);
+        }
+        cnt = cnt + 1;
+    }
+    println!();
+
+}
+
+pub fn q3() {
+
+    let s1 = String::from("パトカー");
+    let s2 = String::from("タクシー");
+    let t1 = s1.chars();
+    let t2 = s2.chars();
+
+    for (i, j) in t1.zip(t2) {
+        print!("{}", i);
+        print!("{}", j);
+    }
+    println!();
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ extern crate nlp100rust;
 fn main() {
 
     nlp100rust::chap01::q1();
+    nlp100rust::chap01::q2();
+    nlp100rust::chap01::q3();
 
 }
