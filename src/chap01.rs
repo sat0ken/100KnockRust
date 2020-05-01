742ccb6b3da9b632ec9f4a9096504c85868342e6
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-01 15:13:48 +0900

    add files

---
+++ src/chap01.rs
@@ -0,0 +1,7 @@
+pub fn q1() {
+
+    let s = String::from("stressed");
+    let t = s.chars().rev().collect::<String>();
+    println!("{}", t);
+
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate nlp100rust;
+
+fn main() {
+
+    nlp100rust::chap01::q1();
+
+}
